cecb9511455c9cba909ed6a1eeb39cc4a1c0180a
Vale42 <valentin.kummert@gmx.de>
2016-01-22 00:27:28 +0100

    Update wakeupbot.py

--- wakeupbot.py
+++ wakeupbot.py
@@ -5,14 +5,16 @@ import time
 
  
 # bot config      
-server =  # Server
-channel = # Channel
-botnick = # Botnickname
+server = "Server" # Server
+channel = "#Channel" # Channel
+botnick = "Botnick" # Botnickname
+spam = 0
+counter = 0
  
 def ping(): # Ping/Pong damit der bot nicht gekickt wird
   ircsock.send("PONG :pingis\n")  
  
-def sendmsg(chan , msg): # Funktion Nachrichtsenden
+def sendmsg(chan , msg): # Funktion Nachricht senden
   ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n")
  
 def joinchan(chan): # Funktion zum channel beitritt
@@ -21,8 +23,15 @@ def joinchan(chan): # Funktion zum channel beitritt
 def hallo(): # Funktion welche Hello sendet
   ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
   
-def wakeup(): #Funktion zum aufrufen einer url (zb für musikplay)
-	urllib.urlopen('*Here URL for Musik launch*')
+def wakeup():
+	#starte sound vom Soundboard
+	urllib.urlopen('#Hier Link zum Soundboardsound')
+	#sleep und wiederholung falls Boxen aus
+	time.sleep(10)
+	urllib.urlopen('Hier Link zum Soundboardsound')
+	#sleep gegen spammer
+	time.sleep(120)
+
                  
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, 6667)) # Zum server verbinden mit dem port: 6667
@@ -40,7 +49,7 @@ while 1: # Vorsicht damit evt endlos schleife
     hallo()
     
   if ircmsg.find(":!wakeup") != -1: # Ruft die Funktion wakeup auf wenn jemand !wakeup schreibt
-    wakeup()
-    
+		wakeup()
+      
   if ircmsg.find("PING :") != -1: # Ruft die Funktion ping auf um dem server zu antworten
     ping()
